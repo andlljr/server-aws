98ceceee0a104d9308b3b43d6c06baaef93d3a65
user.email <99192411+andlljr@users.noreply.github.com>
2023-08-06 16:58:18 -0300

    update client

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const env = require("dotenv").config();
 
 const app = express();
 
-const client = new pg.Client(process.env.URL)
+const client = new pg.Client({
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DB_NAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD
+})
 
 client.connect(function(err){
     if(err){
